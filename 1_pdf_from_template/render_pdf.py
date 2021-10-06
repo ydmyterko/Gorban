82c42545ca5f5e803aa8125d1a830c993ff4a901
ydmyterko <yuriy.dmyterko@gmail.com>
2021-10-06 09:33:52 +0300

    PDF content

--- 1_pdf_from_template/render_pdf.py
+++ 1_pdf_from_template/render_pdf.py
@@ -1,5 +1,7 @@
 import argparse
 from pdf_from_template import TemplatePDF
+import json
+from fpdf import FPDF
 
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 parser.add_argument("--values")
@@ -19,5 +21,37 @@ print(values)
 print(template)
 print(output)
 
-my_PDF = TemplatePDF(values, template, output)
-my_PDF.area()
+with open(values) as f:
+    data = json.load(f)
+    print(data)
+
+json_str = ""
+item_cnt = 0
+pairs = data.items()
+for key, value in pairs:
+    item_cnt += 1
+    print(key, value)
+    json_str += str(item_cnt) + ". " + str(key) + " - " + str(value) + "\n"
+
+# Reading Template file
+with open(template) as file:
+    template_str = file.read()
+print(template_str)
+
+# total string
+total_str = template_str + str(json_str)
+print(total_str)
+
+# creating PDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('Arial', 'B', 10)
+
+# Adding JSON text
+pdf.multi_cell(110, 5, txt=str(total_str))
+
+# Creating output
+pdf.output(output, 'F')
+
+
+
